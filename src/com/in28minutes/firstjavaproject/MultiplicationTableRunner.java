67e29714cc7237b7c17c9be96f91e4265cea7d6e
kanakhan <cjh1950@naver.com>
2023-03-04 23:52:47 +0900

    2

--- src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -4,8 +4,7 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
-		table.print(3, 11, 19);
-		table.print(5);
+		table.print();
 		
 	}
 
